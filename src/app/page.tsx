b70c9a4fdbffcdcd88aca822c5c2155808b30b69
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-08-19 11:14:48 +0300

    edit text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      the website will coming soon
+      the website will coming soon - rami hamadeh company
     </div>
   );
 }
